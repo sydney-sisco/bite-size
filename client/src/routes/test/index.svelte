9babe764a523eb5b58dab9a30a4995cfbf4a5d29
Sydney Sisco <hello@sydsis.co>
2021-04-10 13:21:05 -0700

    proxy is working

---
+++ client/src/routes/test/index.svelte
@@ -0,0 +1,27 @@
+<script>
+  import axios from "axios";
+
+  let name = 'world';
+
+  // let user = {};
+
+  // Make a request for a user with a given ID
+  axios.get('/api/users/10')
+    .then(function (response) {
+      // handle success
+      console.log(response.data[0]);
+
+      name = response.data[0].username;
+
+      // user = {...response.data[0]};
+    })
+    .catch(function (error) {
+      // handle error
+      console.log('the error is:',error);
+    })
+    .then(function () {
+      // always executed
+    });
+</script>
+
+<h2>Hello {name}</h2>
--- client/src/server.js
+++ client/src/server.js
@@ -2,11 +2,24 @@ import sirv from 'sirv';
 import polka from 'polka';
 import compression from 'compression';
 import * as sapper from '@sapper/server';
+const { createProxyMiddleware } = require('http-proxy-middleware');
 
 const { PORT, NODE_ENV } = process.env;
 const dev = NODE_ENV === 'development';
 
-polka() // You can also use Express
+const host = 'localhost:5001';
+
+polka()
+	.use(
+    createProxyMiddleware('/api', {
+      changeOrigin: true,
+      logLevel: 'debug',
+      target: 'http://localhost:5001',
+      pathRewrite: {
+        '^/api': ''
+      }
+    })
+  )
 	.use(
 		compression({ threshold: 0 }),
 		sirv('static', { dev }),

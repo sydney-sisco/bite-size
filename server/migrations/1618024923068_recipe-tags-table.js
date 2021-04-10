70ea4eba40eda833ff7e598842e9b7af0663ad66
Eppa Lea Turniawan <eppalea@gmail.com>
2021-04-09 20:24:14 -0700

    create recipe tags table

---
+++ server/migrations/1618024923068_recipe-tags-table.js
@@ -0,0 +1,29 @@
+/* eslint-disable camelcase */
+
+exports.shorthands = undefined;
+
+exports.up = pgm => {
+  pgm.createTable('recipe_tags', {
+    id: {
+      type: 'SERIAL PRIMARY KEY',
+      notNull: true
+    },
+    recipe_id: {
+      type: 'INTEGER',
+      references: recipes(id),
+      onDelete: 'CASCADE'
+    },
+    tag: {
+      type: 'INTEGER',
+      references: tags(id),
+      onDelete: 'CASCADE'
+    }
+  })
+};
+
+exports.down = pgm => {
+  pgm.dropTable('recipe_tags', {
+    ifExists: true,
+    cascade: true
+  })
+};

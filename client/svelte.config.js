9f7b004031b9442a231587e8cd7da2ae2d0527df
Evan Quirk <evan_quirk@me.com>
2021-04-11 10:51:20 -0700

    style: update attraction theming

--- client/svelte.config.js
+++ client/svelte.config.js
@@ -1,5 +1,16 @@
 const sveltePreprocess = require('svelte-preprocess');
+const makeAttractionsImporter = require('attractions/importer.js');
+const path = require('path')
 
 module.exports = {
-  preprocess: [sveltePreprocess()],
+  preprocess: [sveltePreprocess({
+    scss: {
+      importer: makeAttractionsImporter({
+        // specify the path to your theme file, relative to this file
+        themeFile: path.join(__dirname, 'src/styles/theme.scss'),
+      }),
+      // not mandatory but nice to have for concise imports
+      includePaths: [path.join(__dirname, './src/styles')],
+    },
+  })],
 };

4184771f266d5a2aeb44bd82d2babb4506d15bef
Sydney Sisco <hello@sydsis.co>
2021-04-11 13:52:40 -0700

    create recipeCard component and show recipes on index page using recipeCards

---
+++ client/src/components/RecipecCard.svelte
@@ -0,0 +1,26 @@
+<style>
+	.recipe-card {
+		width: 300px;
+		border: 1px solid #aaa;
+		border-radius: 2px;
+		box-shadow: 2px 2px 8px rgba(0,0,0,0.1);
+		padding: 1em;
+		margin: 0 0 1em 0;
+	}
+
+  .recipe-card:hover {
+    box-shadow: 0 0 11px rgba(33,33,33,.2); 
+  }
+</style>
+
+<script>
+
+  export let recipe;
+
+</script>
+
+<div class="recipe-card">
+  <a href="/recipes/{recipe.id}"><h3>{recipe.title}</h3></a>
+  <p>{recipe.description}</p>
+  <p>created by: {recipe.username}</p>
+</div>
--- client/src/routes/recipes/index.svelte
+++ client/src/routes/recipes/index.svelte
@@ -1,23 +1,60 @@
-<script>
-  import fetch from 'cross-fetch';
+<script context="module">
 
-  async function fetchRecipe() {
-    try {
-      const response = await fetch(`http://localhost:5001/recipes/1`);
-      return response.json();
-      
-    } catch (error) {
-      throw new Error(error);
-    }
+  import RecipeCard from "../../components/RecipecCard.svelte";
+
+  export async function preload(page, session) {
+		const { id } = page.params;
+
+		const res = await this.fetch(`http://localhost:5001/recipes`);
+		const recipeList = await res.json();
+
+		return { recipeList };
 	}
 
+</script>
+
+<script>
+	export let recipeList;
 </script>
 
 <svelte:head>
 	<title>Bite Size</title>
 </svelte:head>
 
-{#await fetchRecipe()}
+<!-- {#each recipeList as { title, description, image_url, username }, id} -->
+{#each recipeList as recipe, id}
+  <!-- <p>{title}</p> -->
+  <RecipeCard recipe={recipe} />
+{/each}
+
+<!-- <h3>{recipeDetails.recipe[0].title}</h3>
+<p>{recipeDetails.recipe[0].description}</p>
+<img style="width: 30%" src="{recipeDetails.recipe[0].image_url}" alt="recipe">
+
+<p>Difficulty: {recipeDetails.recipe[0].difficulty}</p>
+<p>Duration: {recipeDetails.recipe[0].duration} minutes</p>
+<p>Servings: {recipeDetails.recipe[0].servings}</p>
+
+<h3>Ingredients:</h3>
+<ul>
+  {#each recipeDetails.ingredients as { name, unit, quantity }, id}
+    <li>
+      {quantity} x {unit} of {name} 
+    </li>
+  {/each}
+</ul>
+
+<h3>Instructions:</h3>
+<ul>
+  {#each recipeDetails.instructions as { instruction }}
+    <li>
+      {instruction}
+    </li>
+  {/each}
+</ul> -->
+
+
+<!-- {#await fetchRecipe()}
 	<p>...waiting</p>
 {:then recipeData}
   <h3>{recipeData.recipe[0].title}</h3>
@@ -33,4 +70,4 @@
   </ul>
 {:catch error}
 	<p style="color: red">This is the error:{error.message}</p>
-{/await}
+{/await} -->

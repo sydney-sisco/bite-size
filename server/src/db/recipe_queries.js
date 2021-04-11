54c007e90e8272168c9c4a7fa0c8c066c839dba9
Sydney Sisco <hello@sydsis.co>
2021-04-10 17:33:25 -0700

    add recipe queries

--- server/src/db/recipe_queries.js
+++ server/src/db/recipe_queries.js
@@ -7,16 +7,53 @@ const getRecipes = async (fastify) => {
   return rows;
 };
 
-// const getUser = async (fastify, id) => {
+const getRecipe = async (fastify, id) => {
+  const client = await fastify.pg.connect()
+    
+  const { rows } = await client.query(
+    `SELECT * FROM recipes r
+    WHERE r.id=$1`, [id]
+  )
+  client.release()
+
+  return rows;
+}
+
+const getRecipeInstructions = async (fastify, id) => {
+  const client = await fastify.pg.connect()
+    
+  const { rows } = await client.query(
+    `SELECT * FROM instructions
+    WHERE recipe_id = $1
+    ORDER BY step`, [id]
+  )
+  client.release()
+
+  return rows;
+}
+
+// const getRecipeIngredients = async (fastify, id) => {
 //   const client = await fastify.pg.connect()
-//     const { rows } = await client.query(
-//       'SELECT * FROM users WHERE id=$1', [id]
-//     )
-//     client.release()
-//     return rows;
-// };
+    
+//   const { rows } = await client.query(
+//     `SELECT * FROM ingredients
+//     WHERE recipe_id = $1`, [id]
+//   )
+//   client.release()
+
+//   return rows;
+// }
+
+const getRecipeDetails = async (fastify, id) => {
+
+  const recipe = await getRecipe(fastify, id);
+  const instructions = await getRecipeInstructions(fastify, id);
+  // const ingredients = await getRecipeIngredients(fastify, id); // recipe_ingredients table or something 
+
+  return {recipe, instructions};
+};
 
 module.exports = {
   getRecipes,
-  // getUser
+  getRecipeDetails
 }

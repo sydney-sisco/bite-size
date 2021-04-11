647225f1d9609189c14e42ee0bf76fe171b7e707
Sydney Sisco <hello@sydsis.co>
2021-04-11 16:03:31 -0700

    seed emoji reactions and add emoji reactions to recipe details query

--- server/src/db/recipe_queries.js
+++ server/src/db/recipe_queries.js
@@ -1,3 +1,17 @@
+const getUserEmojiReactions = async (fastify, recipe_id) => {
+  const client = await fastify.pg.connect()
+  const { rows } = await client.query(
+    // `select e.name, count(uer.*) as count FROM emojis e
+    // JOIN user_emoji_reactions uer ON e.id = uer.emoji_id
+    // WHERE uer.recipe_id = 1
+    // GROUP BY e.id`
+
+    `select e.name, e.emoji, count(uer.*) from emojis e left join user_emoji_reactions uer on uer.emoji_id = e.id where (uer.recipe_id = 1 or uer.recipe_id is null) group by e.id`
+  )
+  client.release()
+  return rows;
+}
+
 const getRecipes = async (fastify) => {
   const client = await fastify.pg.connect()
   const { rows } = await client.query(
@@ -8,6 +22,9 @@ const getRecipes = async (fastify) => {
     ORDER BY r.id`
   )
   client.release()
+
+  const userEmojiReactions = getUserEmojiReactions(fastify)
+
   return rows;
 };
 
@@ -56,8 +73,9 @@ const getRecipeDetails = async (fastify, id) => {
   const recipe = await getRecipe(fastify, id);
   const instructions = await getRecipeInstructions(fastify, id);
   const ingredients = await getRecipeIngredients(fastify, id); // recipe_ingredients table or something 
+  const emojiReactions = await getUserEmojiReactions(fastify, id); // recipe_ingredients table or something 
 
-  return {recipe, instructions, ingredients};
+  return {recipe, instructions, ingredients, emojiReactions};
 };
 
 module.exports = {

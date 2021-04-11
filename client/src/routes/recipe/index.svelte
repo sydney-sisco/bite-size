3c7761d1b79c90fda8ecdbd6a6dc822f359e6f7a
Sydney Sisco <hello@sydsis.co>
2021-04-10 19:25:07 -0700

    recipe index page loads and displays recipe #1

--- client/src/routes/recipe/index.svelte
+++ client/src/routes/recipe/index.svelte
@@ -17,25 +17,20 @@
 	<title>Bite Size</title>
 </svelte:head>
 
-<h1>Great success!</h1>
-
 {#await fetchRecipe()}
 	<p>...waiting</p>
-{:then instructions}
+{:then recipeData}
+  <h3>{recipeData.recipe[0].title}</h3>
+  <p>{recipeData.recipe[0].description}</p>
+  <img style="width: 30%" src="{recipeData.recipe[0].image_url}" alt="recipe">
+  <h3>Instructions:</h3>
   <ul>
-    {#each instructions as { instruction }}
+    {#each recipeData.instructions as { instruction }}
       <li>
         {instruction}
       </li>
     {/each}
   </ul>
 {:catch error}
-	<p style="color: red">{error.message}</p>
+	<p style="color: red">This is the error:{error.message}</p>
 {/await}
-<!-- 
-<figure>
-	<img alt="Success Kid" src="{successkid}">
-	<figcaption>Have fun with Sapper!</figcaption>
-</figure> -->
-
-<!-- <p><strong>Try  this file (src/routes/index.svelte) TESTINGZ</strong></p> -->

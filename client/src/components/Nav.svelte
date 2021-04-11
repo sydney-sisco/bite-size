6efb21587eaf47844e6ad539483e170723720e8c
Evan Quirk <evan_quirk@me.com>
2021-04-11 10:27:30 -0700

    feat: update navbar with new recipes tab

--- client/src/components/Nav.svelte
+++ client/src/components/Nav.svelte
@@ -56,5 +56,6 @@
 		<!-- for the blog link, we're using rel=prefetch so that Sapper prefetches
 		     the blog data when we hover over the link or tap it on a touchscreen -->
 		<li><a rel=prefetch aria-current="{segment === 'blog' ? 'page' : undefined}" href="blog">blog</a></li>
+		<li><a rel=prefetch aria-current="{segment === 'new recipe' ? 'page' : undefined}" href="new-recipe">new recipe</a></li>
 	</ul>
 </nav>

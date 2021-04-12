f2344b953e97b29a6b35898921057d18b3afaf3e
Sydney Sisco <hello@sydsis.co>
2021-04-12 10:19:03 -0700

    add session routes

--- server/routes/users/index.js
+++ server/routes/users/index.js
@@ -1,4 +1,4 @@
-const { getUsers, getUser } = require('../../src/db/user_queries');
+const { getUsers, getUser, getUserByEmail } = require('../../src/db/user_queries');
 
 
 const usersRoute = async (fastify) => {
@@ -16,6 +16,32 @@ const usersRoute = async (fastify) => {
 
     const rows = await getUser(fastify, req.params.id);
     reply.send(rows);
+  })
+
+  fastify.post('/login', async (req, reply) => {
+    console.log('login details:', req.body); // shows username and password
+
+    const user = await getUserByEmail(fastify, req.body.email);
+
+    console.log(user);
+
+    // req.session.set('data', req.params.id)
+    reply.send('hello world')
+  })
+
+  fastify.get('/test-session', (request, reply) => {
+    const data = request.session.get('data')
+    console.log('session:', data);
+    if (!data) {
+      reply.code(404).send()
+      return
+    }
+    reply.send(data)
+  })
+  
+  fastify.post('/logout', (request, reply) => {
+    request.session.delete()
+    reply.send('logged out')
   })
 }
 

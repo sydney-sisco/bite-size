252c9a817d4fcd4f0b4139b65ca554eecec653d6
Sydney Sisco <hello@sydsis.co>
2021-04-12 14:19:09 -0700

    remove debugging code

--- server/routes/users/index.js
+++ server/routes/users/index.js
@@ -21,25 +21,18 @@ const usersRoute = async (fastify) => {
   })
 
   fastify.post('/login', async (req, reply) => {
-    console.log('login details:', req.body); // shows username and password
-
     const user = await getUserByEmail(fastify, req.body.email);
 
     if (user.length === 0) {
-      console.log('no user found');
       reply.send({error: 'Invalid email.'});
       return;
     }
 
-    console.log('found this user:', user[0]);
-
     if (user[0].password !== req.body.password) { // TODO: hash the passwords at the very least
-      console.log('bad password');
       reply.send({error: 'Invalid Password.'});
       return;
     }
     
-    console.log('good password');
     reply.send({
       user: {...user[0], password: null},
       token: user[0].id,
@@ -47,17 +40,12 @@ const usersRoute = async (fastify) => {
   })
 
   fastify.get('/users/:id/recipes', async (req, reply) => {
-    console.log('fetching user recipes for', req.params.id);
-    console.log(req);
-
+    
     // TODO: check Authorization header somehow
   
     const rows = await getRecipesForUser(fastify, req.params.id);
   
-    console.log('rows:', rows);
-  
     reply.send({recipes: rows});
-
   })
 }
 

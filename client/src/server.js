741af7d4afaae6e38bdcbaa28a530eaa4093149c
Sydney Sisco <hello@sydsis.co>
2021-04-12 10:07:29 -0700

    add session storage to polka

--- client/src/server.js
+++ client/src/server.js
@@ -2,6 +2,11 @@ import sirv from 'sirv';
 import polka from 'polka';
 import compression from 'compression';
 import * as sapper from '@sapper/server';
+import { json } from 'body-parser';
+import session from 'express-session';
+import sessionFileStore from 'session-file-store';
+const FileStore = new sessionFileStore(session);
+
 const { createProxyMiddleware } = require('http-proxy-middleware');
 
 const { PORT, NODE_ENV, API_PORT } = process.env;
@@ -9,6 +14,18 @@ const dev = NODE_ENV === 'development';
 
 polka()
 	.use(
+    json(),
+    session({
+      secret: 'SomeSecretStringThatIsNotInGithub', //TODO: this should also pull from .env
+      resave: true,
+      saveUninitialized: true,
+      cookie: {
+        maxAge: 31536000
+      },
+      store: new FileStore({
+        path: `.sessions`
+      })
+    }),
     createProxyMiddleware('/api', {
       changeOrigin: true,
       logLevel: 'debug',
@@ -21,7 +38,13 @@ polka()
 	.use(
 		compression({ threshold: 0 }),
 		sirv('static', { dev }),
-		sapper.middleware()
+		sapper.middleware({
+      session: (req, res) => {
+        return ({
+          token: req.session.token
+        })
+      }
+    })
 	)
 	.listen(PORT, err => {
 		if (err) console.log('error', err);

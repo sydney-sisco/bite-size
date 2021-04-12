9e93abec52cb3b56f0f73cf06e6f9061f063cea9
Evan Quirk <evan_quirk@me.com>
2021-04-11 22:52:20 -0700

    feat: add recipes post request for creation

--- server/routes/recipes/index.js
+++ server/routes/recipes/index.js
@@ -1,7 +1,7 @@
-const { getRecipeDetails, getRecipes } = require('../../src/db/recipe_queries');
+const { getRecipeDetails, getRecipes , postNewRecipe } = require('../../src/db/recipe_queries');
 
 
-const recipesRoute = async (fastify) => {
+const recipesRoutes = async (fastify) => {
   fastify.get('/recipes', async (req, reply) => {
     const rows = await getRecipes(fastify);
     reply.send(rows);
@@ -13,6 +13,12 @@ const recipesRoute = async (fastify) => {
     // console.log(recipeDetails);
     reply.send(recipeDetails);
   })
+
+  fastify.post('/recipes', async (req, reply) => {
+    const { body } = req
+    await postNewRecipe(fastify, body)
+    reply.code(204)
+  })
 }
 
-module.exports = recipesRoute
+module.exports = recipesRoutes

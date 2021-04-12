c569fd8e62047cc97d37ab1ff1434d67d87da2e6
Sydney Sisco <hello@sydsis.co>
2021-04-12 13:33:03 -0700

    move my recipes page

---
+++ client/src/routes/my-recipes/index.svelte
@@ -0,0 +1,56 @@
+<script context="module">
+  export async function preload(page, session) {
+    const { token, user } = session;
+
+    if (!token) {
+      return this.redirect(302, "login");
+    }
+
+    const response = await this.fetch(`http://localhost:5001/users/${user.id}/recipes`, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+        Authorization: token,
+      },
+    });
+
+    const parsed = await response.json();
+
+    if (parsed.error) {
+      return this.error(response.status, parsed.error);
+    }
+
+    return { recipes: parsed.recipes };
+  }
+</script>
+
+<script>
+  import { stores } from "@sapper/app";
+  
+  import RecipeCard from "../../components/RecipecCard.svelte";
+  
+  const { session } = stores();
+  export let recipes = [];
+
+  console.log('the session is:', $session);
+  
+</script>
+
+<style>
+  .recipe-container {
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    flex-wrap: wrap;
+  }
+</style>
+
+<div class="recipe-container">
+  {#each recipes as recipe, id}
+    <RecipeCard recipe={recipe} />
+  {/each}
+</div>
+
+<p>session id: {$session.token}</p>
+<p>session user.id: {$session.user.id}</p>

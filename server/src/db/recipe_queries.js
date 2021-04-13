edf1225633ad0c9ac8f1d3f399e3f80f31af4055
Eppa Lea Turniawan <eppalea@gmail.com>
2021-04-12 17:54:58 -0700

    capitalize words in some of the queries

--- server/src/db/recipe_queries.js
+++ server/src/db/recipe_queries.js
@@ -33,8 +33,8 @@ const getRecipe = async (fastify, id) => {
     
   const { rows } = await client.query(
     `SELECT r.*, d.name AS difficulty, COUNT(f.*) as favourite_count FROM recipes r
-    JOIN difficulties d on d.id = r.difficulty_id
-    JOIN favourites f on f.recipe_id = r.id
+    LEFT JOIN difficulties d ON d.id = r.difficulty_id
+    LEFT JOIN favourites f ON f.recipe_id = r.id
     WHERE r.id=$1
     GROUP BY r.id, d.name`, [id]
   )
@@ -60,10 +60,10 @@ const getRecipeIngredients = async (fastify, id) => {
   const client = await fastify.pg.connect()
     
   const { rows } = await client.query(
-    `select * from recipe_ingredients ri
-    join ingredients i on i.id = ri.ingredient_id
-    join units_of_measure u on u.id = ri.unit_of_measure_id
-    where ri.recipe_id = $1;`, [id]
+    `SELECT * FROM recipe_ingredients ri
+    LEFT JOIN ingredients i ON i.id = ri.ingredient_id
+    LEFT JOIN units_of_measure u ON u.id = ri.unit_of_measure_id
+    WHERE ri.recipe_id = $1;`, [id]
   )
   client.release()
 

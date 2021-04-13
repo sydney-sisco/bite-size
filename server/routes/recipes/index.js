1c32d38943ddc1785943dec4fe8f88b5f198f698
Sydney Sisco <hello@sydsis.co>
2021-04-13 11:32:38 -0700

    add server routes for adding and removing emoji reactions

--- server/routes/recipes/index.js
+++ server/routes/recipes/index.js
@@ -1,4 +1,5 @@
 const { getRecipeDetails, getRecipes , postNewRecipe , deleteSpecificRecipe } = require('../../src/db/recipe_queries');
+const   { addEmojiReaction, removeEmojiReaction } = require('../../src/db/emoji_queries');
 
 
 const recipesRoutes = async (fastify) => {
@@ -34,6 +35,40 @@ const recipesRoutes = async (fastify) => {
 
     reply.send(deleteRecipe);
   })
+
+  fastify.post('/recipes/:recipe_id/emojis', async (request, reply) => {
+
+    const auth = request.headers.authorization;
+    // console.log('auth:', auth);
+
+    let decoded;
+    if (auth === 'null') {
+      reply.code(403);
+    }
+    const token = auth.split(' ')[1]
+    decoded = fastify.jwt.verify(token);
+    
+    // emoji_id, recipe_id, user_id
+    const addEmoji = await addEmojiReaction(fastify, JSON.parse(request.body).emoji_id, request.params.recipe_id, decoded.id);
+
+    reply.code(204);
+  });
+
+  fastify.delete('/recipes/:recipe_id/emojis/:emoji_id', async (request, reply) => {
+    const auth = request.headers.authorization;
+    // console.log('auth:', auth);
+
+    let decoded;
+    if (auth === 'null') {
+      reply.code(403);
+    }
+    const token = auth.split(' ')[1]
+    decoded = fastify.jwt.verify(token);
+    
+    // emoji_id, recipe_id, user_id
+    const addEmoji = await removeEmojiReaction(fastify, request.params.emoji_id, request.params.recipe_id, decoded.id);
+    reply.code(204);
+  });
 }
 
 module.exports = recipesRoutes

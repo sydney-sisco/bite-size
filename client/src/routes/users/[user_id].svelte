9607fd210315036f3ed07834bb9159735fbc47a8
Eppa Lea Turniawan <eppalea@gmail.com>
2021-04-14 12:13:06 -0700

    create user_id page for user specific recipes

---
+++ client/src/routes/users/[user_id].svelte
@@ -0,0 +1,33 @@
+<script context="module">
+
+  export async function preload(page, session) {
+		
+		const res = await this.fetch(`http://localhost:5001/users/${user_id}/recipes`, 
+			{
+				method: "GET",
+			}
+		);
+		const recipeDetails = await res.json();
+	
+    console.log('[user_id] recipe details:', recipeDetails)
+		return { recipeDetails };
+	}
+	
+</script>
+
+<script>
+  
+  import RecipeCard from '../../components/RecipecCard.svelte';
+  export let recipeDetails;
+  
+</script>
+
+<svelte:head>
+  <title>Bite Size</title>
+</svelte:head>
+
+<div class="recipe-container">
+  {#each recipeDetails as recipe}
+    <RecipeCard recipe={recipe} />
+  {/each}
+</div>

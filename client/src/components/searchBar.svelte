d47a74aae4dd24883d18a1622c8e490c313c4ed7
Evan Quirk <evan_quirk@me.com>
2021-04-14 09:30:10 -0700

    feat: add searchBar component to homepage

---
+++ client/src/components/searchBar.svelte
@@ -0,0 +1,23 @@
+<script context="module">
+  export async function preload(page, session) {
+		const res = await this.fetch(`http://localhost:5001/search`);
+		const recipeList = await res.json();
+
+		return {  };
+	}
+</script>
+
+<script>
+  import { Autocomplete } from 'attractions'
+
+  async function* getOptions(text) {
+    await sleep(1000);
+    yield [
+      { name: text, details: 'Optional' },
+      { name: `it highlights the match: ${text}` },
+      { name: `show me ${text}` }
+    ];
+  }
+</script>
+
+<Autocomplete {getOptions}/>

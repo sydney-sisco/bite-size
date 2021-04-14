f90e40751f97f3c237bf722b266e68a25190b74a
Sydney Sisco <hello@sydsis.co>
2021-04-14 10:59:03 -0700

    update recipe show page to use 2-column layout

--- client/src/routes/recipes/index.svelte
+++ client/src/routes/recipes/index.svelte
@@ -1,4 +1,29 @@
 <style>
+  main {
+    display: flex;
+    width: 100%;
+    justify-content: space-between
+  }
+
+  .left {
+    width: 67%;
+    max-width: 816px;
+  }
+
+  .right {
+    background-color: #FFF;
+    width: 30%;
+    margin-top: 3em;
+    max-width: 336px;
+    height: 0%;
+  }
+
+  .right h2 {
+    background-color: #000;
+    color: #FF0;
+    padding-left: 1em;
+  }
+
   .recipe-container {
     display: flex;
     flex-direction: row;
@@ -32,8 +57,24 @@
 	<title>Bite Size</title>
 </svelte:head>
 
-<div class="recipe-container">
-  {#each recipeList as recipe, id}
-    <RecipeCard recipe={recipe} />
-  {/each}
-</div>
+<main>
+  <div class="left">
+    <h1>Recipes</h1>
+
+    <div class="recipe-container">
+      {#each recipeList as recipe, id}
+        <RecipeCard recipe={recipe} />
+      {/each}
+    </div>
+  </div>
+  <div class="right">
+    <h2>Filters</h2>
+    <ul>
+      {#each [...Array(10).keys()] as i}
+        <li>placeholder</li>
+      {/each}
+    </ul>  
+  </div>
+</main>
+
+

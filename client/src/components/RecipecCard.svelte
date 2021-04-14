7e96f3c9fcdccefccee4c94dd5f273ca8aba25e2
Sydney Sisco <hello@sydsis.co>
2021-04-13 23:11:30 -0700

    add hover effect to recipe cards

--- client/src/components/RecipecCard.svelte
+++ client/src/components/RecipecCard.svelte
@@ -1,17 +1,26 @@
 <style>
 	.recipe-card {
 		width: 300px;
-		border: 1px solid #aaa;
-		border-radius: 2px;
-		box-shadow: 2px 2px 8px rgba(0,0,0,0.1);
+		border: 1px solid #000;
+		/* border-radius: 2px; */
+		/* box-shadow: 2px 2px 8px rgba(95, 81, 81, 0.1); */
 		padding: 1em;
 		margin: 0 0 1em 0;
     width: 25%;
+    font-weight: 400;
+
+    transition: transform .1s, box-shadow .1s;
+    /* transition-timing-function: ease; */
+    transition-timing-function: ease-out;
 	}
 
   .recipe-card:hover {
-    box-shadow: 0 0 11px rgba(33,33,33,.2); 
+    /* box-shadow: 0 0 11px rgba(33,33,33,.2);  */
+    /* box-shadow: 0px 10px 10px 0px; */
+    box-shadow: -5px 5px #000;
+    transform: translate(5px, -5px);
   }
+
 </style>
 
 <script>

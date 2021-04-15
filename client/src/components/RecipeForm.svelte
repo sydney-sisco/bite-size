c5bc292c670e9a09558c4e65f3f782c0c34c12b1
Eppa Lea Turniawan <eppalea@gmail.com>
2021-04-15 13:09:12 -0700

    clean up recipe form

--- client/src/components/RecipeForm.svelte
+++ client/src/components/RecipeForm.svelte
@@ -23,6 +23,8 @@
 
   export let recipe;
   console.log('recipe obj', recipe);
+
+
   
   let { 
     difficulty_id = 2,
@@ -31,10 +33,19 @@
     duration = 0,
     image_url = null,
     servings = null,
-    instructionSteps = ["", "", ""],
+    // instructionSteps = ["", "", ""],
     unitOfMeasure = 1,
     quantity = 1,
   } = recipe[0];
+
+  // format instructions for the textarea
+  export let instructions;
+  let instructionSteps = ''
+
+  for (let {instruction} of instructions) {
+    instructionSteps += `${instruction}\n`;
+  }
+
 
   // format ingredient list for the textarea
   export let ingredients;
@@ -76,28 +87,6 @@
     const changePhoto = () => {
       image_url = null;
     };
-    
-    const addStep = () => {
-      instructionSteps.push("");
-      instructionSteps = instructionSteps;
-      console.log(instructionSteps);
-    };
-    
-    const removeStep = () => {
-      instructionSteps.pop("");
-      instructionSteps = instructionSteps;
-    };
-    
-    const addIngredient = () => {
-      ingredientList.push("");
-      ingredientList = ingredientList;
-      console.log(ingredientList);
-    };
-
-  const removeIngredient = () => {
-    ingredientList.pop("");
-    ingredientList = ingredientList;
-  };
 
   const items = [
     { value: 1, label: "Vegetarian" },
@@ -117,100 +106,105 @@
   ];
 </script>
 
-<Headline>Create a New Recipe</Headline>
-  <FormField name="Recipe Title" required>
-    <TextField bind:value={title} />
-  </FormField>
-  <FormField name="Description">
-    <TextField bind:value={description} />
-  </FormField>
-  <FormField name="Total Duration">
-    Hours: <input
-      type="number"
-      bind:value={hours}
-      id="hours"
-      name="hours"
-      min="0"
-      max="72"
-    />
-    Minutes:
-    <input
-      type="number"
-      bind:value={minutes}
-      id="minutes"
-      name="minutes"
-      min="0"
-      max="59"
-    />
-  </FormField>
-  <FormField name="Servings">
-    <input
-      type="number"
-      bind:value={servings}
-      id="servings"
-      name="servings"
-      min="1"
-      max="50"
-    />
-  </FormField>
+<main>
+  <Headline>Create a New Recipe</Headline>
+    <FormField name="Recipe Title" required>
+      <TextField bind:value={title} />
+    </FormField>
 
-  <FormField name="Difficulty">
-    <input
-      type="range"
-      min="1"
-      max="3"
-      bind:value={difficulty_id}
-      class="slider"
-      name="difficulty-slider"
-    />
-    <output for="difficulty-slider">{difficultyNames[difficulty_id - 1]}</output>
-  </FormField>
+    <FormField name="Description">
+      <TextField bind:value={description} />
+    </FormField>
 
-  <!-- {#each ingredientList as ingredient, index} -->
+    <FormField name="Total Duration">
+      Hours: 
+        <input
+          type="number"
+          bind:value={hours}
+          id="hours"
+          name="hours"
+          min="0"
+          max="72"
+        />
+      Minutes: 
+        <input
+          type="number"
+          bind:value={minutes}
+          id="minutes"
+          name="minutes"
+          min="0"
+          max="59"
+        />
+    </FormField>
+
+    <FormField name="Servings">
+      <input
+        type="number"
+        bind:value={servings}
+        id="servings"
+        name="servings"
+        min="1"
+        max="50"
+      />
+    </FormField>
+
+    <FormField name="Difficulty">
+      <input
+        type="range"
+        min="1"
+        max="3"
+        bind:value={difficulty_id}
+        class="slider"
+        name="difficulty-slider"
+      />
+      <output for="difficulty-slider">
+        {difficultyNames[difficulty_id - 1]}
+      </output>
+    </FormField>
+
     <FormField name="Ingredients" required>
-      <!-- <TextField bind:value={ingredientList[index]} /> -->
-
-      <textarea rows="8" cols="50" name="Ingredients" bind:value={ingredientList} placeholder="Put each ingredient on its own line."></textarea>
-
+        <textarea 
+          rows="8" 
+          cols="50" 
+          name="Ingredients" 
+          bind:value={ingredientList} 
+          placeholder="Put each ingredient on its own line."></textarea>
     </FormField>
-  <!-- {/each} -->
 
-  <!-- <Button on:click={addIngredient}>Add Ingredient</Button> -->
-  <!-- <Button on:click={removeIngredient}>Remove</Button> -->
+    {#if !image_url}
+      <FileDropzone accept="image/*" max={1} on:change={uploadImage}>
+        <span slot='empty-layer'>Choose an Image</span>
+      </FileDropzone>
+    {:else}
+      <img src={image_url} alt="recipe" />
+      <Button on:click={changePhoto}>Pick a different photo</Button>
+    {/if}
 
-  {#if !image_url}
-    <FileDropzone accept="image/*" max={1} on:change={uploadImage}>
-      <span slot='empty-layer'>Choose an Image</span>
-    </FileDropzone>
-  {:else}
-    <img src={image_url} alt="recipe" />
-    <Button on:click={changePhoto}>Pick a different photo</Button>
-  {/if}
+    <CheckboxChipGroup {items} name="tags" outline />
 
-  <CheckboxChipGroup {items} name="tags" outline />
-
-  {#each instructionSteps as step, index}
     <FormField name="Instructions" required>
-      <TextField bind:value={instructionSteps[index]} />
+      <textarea 
+        rows="8" 
+        cols="50" 
+        name="Instructions" 
+        bind:value={instructionSteps} 
+        placeholder="Put each instruction on its own line."></textarea>
     </FormField>
-  {/each}
 
-  <Button on:click={addStep}>Add Another Step</Button>
-  {#if ingredientList.length > 0}
-  <Button on:click={removeStep}>Remove Step</Button>
-  {/if}
-  <Button on:click={() => handleCancel()}>Cancel</Button>
-  <Button filled class="btn" on:click={() => handleSubmit({
-    userId: $session.user.id,
-    title,
-    difficulty_id,
-    hours,
-    minutes,
-    image_url,
-    servings,
-    description,
-    instructionSteps,
-    ingredientList,
-    unitOfMeasure,
-    quantity
-  }, recipe[0].id)}>{!loadingState ? 'Submit Recipe' : 'Loading...'}</Button>
+    <Button on:click={() => handleCancel()}>Cancel</Button>
+    
+    <Button filled class="btn" on:click={() => handleSubmit({
+      userId: $session.user.id,
+      title,
+      difficulty_id,
+      hours,
+      minutes,
+      image_url,
+      servings,
+      description,
+      instructionSteps,
+      ingredientList,
+      unitOfMeasure,
+      quantity
+    }, recipe[0].id)}>{!loadingState ? 'Submit Recipe' : 'Loading...'}</Button>
+</main>

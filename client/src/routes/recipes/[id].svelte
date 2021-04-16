83fe50fdb3860f45a7d575bdf85401192da46850
Eppa Lea Turniawan <eppalea@gmail.com>
2021-04-15 22:43:24 -0700

    remove commented code

--- client/src/routes/recipes/[id].svelte
+++ client/src/routes/recipes/[id].svelte
@@ -277,17 +277,6 @@
   };
 
   import formatIngredients from '../../helpers/helpers';
-  // const formatIngredients = (name, unit, quantity) => {       
-  //   if (unit === 'each') {
-  //     return `${quantity} ${name}`
-  //   }
-    
-  //   if (quantity === 1) {
-  //     return `${quantity} ${unit} of ${name}` 
-  //   }
-              
-  //   return `${quantity} ${unit}s of ${name}`
-  // }
 
 </script>
 

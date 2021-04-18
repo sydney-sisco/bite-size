ea221f4e69942c7a68090d27e34ea9e5d34c5896
Sydney Sisco <hello@sydsis.co>
2021-04-18 12:51:54 -0700

    change search options to increase accuracy of results

--- client/src/components/searchBar.svelte
+++ client/src/components/searchBar.svelte
@@ -36,7 +36,7 @@
 		// findAllMatches: false,
 		// minMatchCharLength: 1,
 		// location: 0,
-		threshold: 0.6,
+		threshold: 0.4,
 		// distance: 100,
 		// useExtendedSearch: false,
 		// ignoreLocation: false,

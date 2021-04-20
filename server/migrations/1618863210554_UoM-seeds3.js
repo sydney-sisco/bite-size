d4c3ba8ac72a163e96d8b4f3eba3ce8054cf0c24
Eppa Lea Turniawan <eppalea@gmail.com>
2021-04-20 14:51:23 -0700

    add more seeds to units of measure

--- server/migrations/1618863210554_UoM-seeds3.js
+++ server/migrations/1618863210554_UoM-seeds3.js
@@ -2,6 +2,21 @@
 
 exports.shorthands = undefined;
 
-exports.up = pgm => {};
+exports.up = pgm => {
 
-exports.down = pgm => {};
+  pgm.sql(`INSERT INTO units_of_measure (unit) VALUES ('slice');
+  INSERT INTO units_of_measure (unit) VALUES ('bottle');
+  INSERT INTO units_of_measure (unit) VALUES ('clove');
+  INSERT INTO units_of_measure (unit) VALUES ('dash');
+  INSERT INTO units_of_measure (unit) VALUES ('sprig');
+  INSERT INTO units_of_measure (unit) VALUES ('bunch');
+  INSERT INTO units_of_measure (unit) VALUES ('piece');
+  INSERT INTO units_of_measure (unit) VALUES ('strip');
+ `)
+
+};
+
+
+exports.down = pgm => {
+  pgm.sql(`DELETE FROM units_of_measure`)
+};

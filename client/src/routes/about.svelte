e9d9792282669a76e020e2236d4a22a5a33a33f7
Eppa Lea Turniawan <eppalea@gmail.com>
2021-04-20 20:53:20 -0700

    create about page

--- client/src/routes/about.svelte
+++ client/src/routes/about.svelte
@@ -1,44 +1,23 @@
-<script context="module">
+<!-- <script context="module">
 
-  import RecipeCard from "../components/RecipecCard.svelte";
 
-	import logo from "../../static/logo.png";
-  
-  export async function preload(page, session) {
-		const res = await this.fetch(`${session.server}`, 
-			{
-				method: "GET",
-			}
-		);
-		const recipeList = await res.json();
 	
-		return { recipeList };
-	}
 	
-</script>
+</script> -->
 
 <script>
-  import { Button } from 'attractions';
-	import { goto } from '@sapper/app';
-  import { fly } from 'svelte/transition';
+	
+	import logo from "../../static/logo.png";
+	import sapper_logo from "../../static/Sapper.png";
+	import svelte_logo from "../../static/svelte.png";
+	import fastify_logo from "../../static/fastify-logo.png";
+	import syd from "../../static/syd.png";
+	import evan from "../../static/evan.png";
+	// import eppalea from "../../static/eppalea.png";
+	import eppalea from "../../static/EL2.png";
+	import github from "../../static/githublogo.png";
   
-  export let recipeList;
 
-  
-  let featuredRecipes = false;
-  const showFeatured = async () => {
-	featuredRecipes ? featuredRecipes = false : featuredRecipes = true; {
-	  }	
-  }
-
-	let searchResults = [];
-
-	let searchTerm = '';
-
-	const submitSearch = searchTerm => {
-		console.log('??', searchTerm);
-		goto(`/recipes?search=${searchTerm}`);
-	}
 
 </script>
 
@@ -67,6 +46,43 @@
 		font-size: 16pt;
 	}
 
+	.stack {
+		/* border: 2px solid blue; */
+		display: flex;
+		height: 175px;
+    margin: 25px;
+	}
+
+	.team {
+		/* border: 2px solid red; */
+		display: flex;
+	}
+/* 
+	.evan {
+		border:  2px solid green;
+	}
+
+	.syd {
+		border:  2px solid black;
+	} */
+
+	.eppalea {
+		/* border: 2px solid purple; */
+		width: 464px;
+		height: 256.23px;
+		
+	}
+
+	.el-img {
+		height: 137.99px;
+		width: 137.99px;
+	}
+
+	.el-gh {
+		height: 40.57px;
+		width: 137.99px;
+	}
+
 	
 </style>
 
@@ -78,6 +94,29 @@
 	<img src="{logo}" alt="bite size logo">
 
 	<p>a more digestible recipe site</p>
+
+	<div class="stack"> 
+		<img src="{svelte_logo}" alt="svelte logo">
+		<img src="{sapper_logo}" alt="sapper logo">
+		<img src="{fastify_logo}" alt="fastify logo">
+	</div>
+	<div class="team">
+		<div class="evan">
+			<img src="{evan}" alt="evan profile">
+			<img src="{github}" alt="github logo">
+			<p>evanquirk</p>
+		</div>
+		<div class="syd">
+			<img src="{syd}" alt="syd profile">
+			<img src="{github}" alt="github logo">
+			<p>sydney-sisco</p>
+		</div>
+		<div class="eppalea">
+			<img class="el-img" src="{eppalea}" alt="eppalea profile">
+			<img class="el-gh" src="{github}" alt="github logo">
+			<p>eppalea</p>
+		</div>
+	</div>
 
 </main>
 	

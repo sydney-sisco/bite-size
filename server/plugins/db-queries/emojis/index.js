53e6c6019630f080257f68262933d63004faf23f
Evan Quirk <evan_quirk@me.com>
2021-05-11 12:59:20 -0700

    refactor: move emoji queries to plug in

---
+++ server/plugins/db-queries/emojis/index.js
@@ -0,0 +1,35 @@
+const fastifyPlugin = require('fastify-plugin')
+
+async function emojiQueries (fastify) {
+  const { pg: { query } } = fastify
+
+  fastify.decorate('emojiQuery', {
+    addReaction: async (emojiId, recipeId, userId) => {
+      return query(`
+        INSERT INTO user_emoji_reactions (emoji_id, recipe_id, user_id)
+        VALUES ($1, $2, $3)
+        RETURNING *;
+        `, [emojiId, recipeId, userId]
+      )
+    },
+      removeReaction: async (emojiId, recipeId, userId) => {
+        return query(`
+          DELETE FROM user_emoji_reactions
+          WHERE emoji_id = $1
+          AND recipe_id = $2
+          AND user_id = $3
+          `, [emojiId, recipeId, userId]
+      )
+    }
+  })
+} 
+
+module.exports = fastifyPlugin(emojiQueries, {
+  name: 'emoji-queries',
+  decorators: {
+    fastify: ['pg']
+  },
+  dependencies: [
+    'fastify-postgres'
+  ]
+})

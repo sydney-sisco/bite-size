f3074b9489a7827f5c5113db7de9ff24e7220b6c
Evan Quirk <evan_quirk@me.com>
2021-05-13 21:22:20 -0700

    refactor: remove example file for plugin

--- server/routes/example/index.js
+++
@@ -1,9 +0,0 @@
-const exampleRoute = async (fastify) => {
-  fastify.get('/example', async (req, reply) => {
-    reply.send({
-      hello: 'world'
-    })
-  })
-}
-
-module.exports = exampleRoute

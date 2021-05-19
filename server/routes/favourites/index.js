0dc473c14f288a7cae2550236a91ce2587430be0
Evan Quirk <evan_quirk@me.com>
2021-05-18 21:36:57 -0700

    refactor: alter favourites call to use function

--- server/routes/favourites/index.js
+++ server/routes/favourites/index.js
@@ -1,9 +1,6 @@
-const { getUserFavourites, addUserFavourite, deleteUserFavourite } = require("../../src/db/favourites_queries")
-const { getRecipeTags } = require("../../src/db/recipe_queries")
-
 const favouritesRoutes = async (fastify) => {
   fastify.get('/users/:user_id/favourites', async (req, reply) => {
-    const rows = await getUserFavourites(fastify, req.params.user_id);
+    const rows = await favouriteQuery.getUserFavourites(fastify, req.params.user_id);
     const recipesWithTags = []
     for (recipe of rows) {
       recipe.tag = await getRecipeTags(fastify, recipe.id)
@@ -15,14 +12,14 @@ const favouritesRoutes = async (fastify) => {
   fastify.post('/users/:user_id/favourites', async (req, reply) => {
     const { body } = req
     console.log('post body', body)
-    await addUserFavourite(fastify, req.params.user_id, body.recipe_id)
+    await favouriteQuery.addUserFavourite(fastify, req.params.user_id, body.recipe_id)
     reply.code(204)
   })
 
   fastify.delete('/users/:user_id/favourites/:recipe_id', async (req, reply) => {
     const { body } = req
     console.log('delete body', body)
-    const deleteFavourite = await deleteUserFavourite(fastify, req.params.user_id, req.params.recipe_id);
+    const deleteFavourite = await favouriteQuery.deleteUserFvourite(fastify, req.params.user_id, req.params.recipe_id);
     reply.send(deleteFavourite);
   })
 

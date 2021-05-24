9c302b09f32f725a18262e136ec5e38399aecee0
Evan Quirk <evan_quirk@me.com>
2021-05-23 22:32:36 -0700

    refactor: update search route with new plugin decorator and remove redundancies

--- server/routes/search/index.js
+++ server/routes/search/index.js
@@ -1,16 +1,9 @@
 const searchRoute = async (fastify) => {
   const { searchQuery } = fastify
   fastify.get('/preload/search', async (req, reply) => {
-    const recipes = await searchQuery.allRecipes()
-    // const ingredients = await searchQuery.allIngredients()
-    const tags = await searchQuery.allTags()
-    // console.log(recipes, ingredients, tags);
-    // console.log(recipes, ingredients, tags);
-    reply.send({
-      recipes,
-      // ingredients,
-      tags
-    })
+    const { rows: recipes } = await searchQuery.allRecipes()
+    const { rows: tags } = await searchQuery.allTags()
+    reply.send({ recipes, tags })
   })
 }
 

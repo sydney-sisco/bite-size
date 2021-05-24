b3fabbe5b7af496c99ba9a78db59b088fdd3bb8b
Evan Quirk <evan_quirk@me.com>
2021-05-23 22:32:06 -0700

    refactor: update recipe route with new plugin decorator and remove redundancies

--- server/routes/recipes/index.js
+++ server/routes/recipes/index.js
@@ -1,79 +1,83 @@
 const recipesRoutes = async (fastify) => {
-  const { recipeQuery } = fastify
+  const { recipeQuery, emojiQuery } = fastify
 
   fastify.get('/recipes', async (req, reply) => {
-    const allRecipes = await recipeQuery.getRecipes();
+    const { rows } = await recipeQuery.getRecipes()
     const recipesWithTags = []
-    for (recipe of allRecipes) {
-      recipe.tag = await recipeQuery.getTags(recipe.id)
+    for (const recipe of rows) {
+      const { rows } = await recipeQuery.getTags(recipe.id)
+      recipe.tag = rows
       recipesWithTags.push(recipe)
     }
-    reply.send(recipesWithTags);
+    reply.send(recipesWithTags)
   })
 
-  fastify.get('/recipes/:id', async (request, reply) => {
-
-    const auth = request.headers.authorization;
-    let decoded;
-    if (auth && auth !== 'null') {
-      const token = auth.split(' ')[1]
-      decoded = fastify.jwt.verify(token);
+  fastify.get('/recipes/:id', async (req, reply) => {
+    const { headers: { authorization }, params: { id } } = req
+    let decoded
+    if (authorization && authorization !== 'null') {
+      const token = authorization.split(' ')[1]
+      decoded = fastify.jwt.verify(token)
     }
 
-    const recipeDetails = await recipeQuery.getRecipeDetails(fastify, request.params.id, decoded && decoded.id);
+    const recipeDetails = await recipeQuery.getRecipeDetails(id, decoded && decoded.id)
 
-    reply.send(recipeDetails);
+    reply.send(recipeDetails)
   })
 
   fastify.post('/recipes', async (req, reply) => {
     const { body } = req
-    const newRecipe = await recipeQuery.postNewRecipe(body)
-    reply.send(newRecipe)
+    const { rows } = await recipeQuery.postNewRecipe(body)
+    reply.send(rows)
   })
-               
+  
+  // TODO: fix the double query nonsense
   fastify.patch('/recipes/:id', async (req, reply) => {
-    const { body } = req
-    const editRecipe = await recipeQuery.editRecipe(body, req.params.id);
-    console.log('editRecipe', editRecipe);
-    const recipeDetails = await getRecipeDetails(fastify, req.params.id);
-    reply.send(recipeDetails);
+    const { body, params: { id } } = req
+    await recipeQuery.editRecipe(body , id)
+    const { rows } = await recipeQuery.getRecipeDetails(id)
+    reply.send(rows)
   })
 
   fastify.delete('/recipes/:id', async (req, reply) => {
-    const deleteRecipe = await deleteSpecificRecipe(fastify, req.params.id);
-
-    reply.send(deleteRecipe);
+    const { params: { id } } = req
+    const { rows } = await recipeQuery.deleteSpecificRecipe(id)
+    reply.send(rows)
   })
 
-  fastify.post('/recipes/:recipe_id/emojis', async (request, reply) => {
-
-    const auth = request.headers.authorization;
-  
-    let decoded;
-    if (auth === 'null') {
-      reply.code(403);
-    }
-    const token = auth.split(' ')[1]
-    decoded = fastify.jwt.verify(token);
-
-    const addEmoji = await addEmojiReaction(fastify, JSON.parse(request.body).emoji_id, request.params.recipe_id, decoded.id);
-
-    reply.code(204);
-  });
-
-  fastify.delete('/recipes/:recipe_id/emojis/:emoji_id', async (request, reply) => {
-    const auth = request.headers.authorization;
-
-    let decoded;
-    if (auth === 'null') {
-      reply.code(403);
-    }
-    const token = auth.split(' ')[1]
-    decoded = fastify.jwt.verify(token);
+  fastify.post('/recipes/:recipeId/emojis', async (req, reply) => {
+    const {
+      headers: { authorization },
+      params: { recipeId },
+      body
+    } = req
     
-    const addEmoji = await removeEmojiReaction(fastify, request.params.emoji_id, request.params.recipe_id, decoded.id);
-    reply.code(204);
-  });
+    if (authorization === 'null') {
+      reply.code(403)
+    }
+    const token = auth.split(' ')[1]
+    const decoded = fastify.jwt.verify(token)
+
+    await emojiQuery.addReaction(JSON.parse(body).emoji_id, recipeId, decoded.id)
+
+    //  TODO: respond with something more useful
+    reply.code(204)
+  })
+
+  fastify.delete('/recipes/:recipeId/emojis/:emojiId', async (req, reply) => {
+    const { headers: { authorization }, params: { recipeId, emojiId } } = req
+
+    if (authorization === 'null') {
+      reply.code(403)
+    }
+
+    const token = auth.split(' ')[1]
+    const decoded = fastify.jwt.verify(token)
+
+    await emojiQuery.removeReaction(emojiId, recipeId, decoded.id)
+
+    reply.code(204)
+  })
 }
 
 module.exports = recipesRoutes

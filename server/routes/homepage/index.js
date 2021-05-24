fe321c07ab8bd7aa452d5d3aa3bc9f85067e8990
Evan Quirk <evan_quirk@me.com>
2021-05-23 22:31:34 -0700

    refactor: update homepage route with new plugin decorator, and remove redundancies

--- server/routes/homepage/index.js
+++ server/routes/homepage/index.js
@@ -1,16 +1,18 @@
-
 const homepageRoute = async (fastify) => {
-  const { recipeQuery } = fastify;
+  const { recipeQuery } = fastify
+
   fastify.get('/', async (req, reply) => {
-    const { rows } = await recipeQuery.favsForFeatured();
+    const { rows } = await recipeQuery.favsForFeatured()
+    
     const recipesWithTags = []
-    for (recipe of rows) {
-      recipe.tag = await recipeQuery.getTags(fastify, recipe.id)
+    for (const recipe of rows) {
+      const { rows } = await recipeQuery.getTags(recipe.id)
+      recipe.tag = rows
       recipesWithTags.push(recipe)
     }
-    reply.send(recipesWithTags);
-    // reply.send(rows);
+
+    reply.send(recipesWithTags)
   })
 }
 
-module.exports = homepageRoute;
+module.exports = homepageRoute
